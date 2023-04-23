17f2924cd2789877ccc212b1634394e62a4f4584
sonsbe <62368303+sonsbe@users.noreply.github.com>
2023-04-24 05:31:51 +0900

    Add files via upload

--- src/pages/Result.js
+++ src/pages/Result.js
@@ -4,21 +4,25 @@ import RelatedLink from "../components/Result/RelatedLink";
 import SelectedList from "../components/Result/SelectedList";
 import Styling from "../components/Result/Styling";
 import './Result.css';
+import { useState } from "react";
 const Result = () => {
+    const [selectedProducts, setSelectedProducts] = useState([null, null, null, null]);
+
+
     return (
         <div className="page-content">
         <div className="left-section">
             <Ad />
         </div>
         <div className="center-section">
-            <Styling />
+            <Styling setSelectedProducts={setSelectedProducts}/>
         </div>
         <div className="right-section">
             <RelatedImage />
             <RelatedLink />
         </div>
         <div className="bottom-section">
-            <SelectedList />
+            <SelectedList selectedProducts={selectedProducts}/>
         </div>
     </div>
     )
